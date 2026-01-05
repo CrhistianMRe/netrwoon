e44d1586de5c756ec8886e73ec6164945563fd71
CrhistianMRe <came.29meca@gmail.com>
2026-01-05 14:08:53 +0800

    feat: add base open menu action

---
+++ src/main/java/com/crhistianm/netrwoon/actions/OpenNetrwoonAction.java
@@ -0,0 +1,16 @@
+package com.crhistianm.netrwoon.actions;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class OpenNetrwoonAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent arg0) {
+        
+    }
+    
+}
+
