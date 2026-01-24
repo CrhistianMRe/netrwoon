ec1b0aa9cacd7c6d1f82cd6c2bfce4c4c120e514
CrhistianMRe <came.29meca@gmail.com>
2026-01-24 19:45:21 +0800

    feat: add base view components

---
+++ src/main/java/com/crhistianm/netrwoon/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.crhistianm.netrwoon.utils;
+
+public class Constants {
+
+    public static final String MAIN_VIEW_NAME = "Netrwoon";
+
+    public static final String HELP_LABEL_TEXT = "Quick Help: -:go up dir  D:delete  R:rename %:create file d:create directory esc:exit";
+
+    
+}
