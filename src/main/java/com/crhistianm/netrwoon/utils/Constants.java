67fbcdc7f3e851de0e23125c715c43a76af56c87
CrhistianMRe <came.29meca@gmail.com>
2026-01-27 13:31:41 +0800

    feat: add window size settings

--- src/main/java/com/crhistianm/netrwoon/utils/Constants.java
+++ src/main/java/com/crhistianm/netrwoon/utils/Constants.java
@@ -6,5 +6,6 @@ public class Constants {
 
     public static final String HELP_LABEL_TEXT = "Quick Help: -:go up dir  D:delete  R:rename %:create file d:create directory esc:exit";
 
+    public static final String SETTINGS_HEADER = "Netrwoon Settings";
     
 }
