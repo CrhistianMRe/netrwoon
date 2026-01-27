687ae965727693042d9728d07e3c61c6ec5ebc00
CrhistianMRe <came.29meca@gmail.com>
2026-01-27 18:52:31 +0800

    feat: add exit/escape main view list action

--- src/main/java/com/crhistianm/netrwoon/controllers/KeyBinds.java
+++ src/main/java/com/crhistianm/netrwoon/controllers/KeyBinds.java
@@ -9,11 +9,12 @@ class KeyBinds {
 
     static InputMap getMainViewKeyBinds() {
         InputMap im = new InputMap();
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "escape");
         im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0), "goInto");
         im.put(KeyStroke.getKeyStroke('-'), "goBack");
         im.put(KeyStroke.getKeyStroke('k'), "moveUp");
         im.put(KeyStroke.getKeyStroke('j'), "moveDown");
         return im;
     }
-
+    
 }
