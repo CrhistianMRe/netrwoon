7173802e3e60a3ae1e0dd99fd00cebd4df5976e1
CrhistianMRe <came.29meca@gmail.com>
2026-02-16 09:06:29 +0800

    ops: add release preparation workflow

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+<!-- Keep a Changelog guide -> https://keepachangelog.com -->
+
+# Netrwoon Changelog
+
+## [Unreleased]
+
+## [1.0.0] - 2026-02-16
+
+### Added 
+
+- Basic functionality that Netrw can do
+
+[UNRELEASED]: https://github.com/CrhistianMRe/netrwoon/compare/v1.0.0...HEAD
+
+[1.0.0]: https://github.com/CrhistianMRe/netrwoon/commits/v1.0.0
+
+
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,13 @@
+import org.jetbrains.changelog.Changelog
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.1.0"
     id("org.jetbrains.intellij.platform") version "2.11.0"
+    id("org.jetbrains.changelog") version "2.0.0"
 }
 
-group = "com.crhistianm"
-version = "1.0-SNAPSHOT"
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
 
 repositories {
     mavenCentral()
@@ -21,8 +23,7 @@ dependencies {
         create("IC", "2025.1")
         testFramework(org.jetbrains.intellij.platform.gradle.TestFrameworkType.Platform)
 
-        // Add necessary plugin dependencies for compilation here, example:
-        // bundledPlugin("com.intellij.java")
+     
     }
 }
 
@@ -32,10 +33,39 @@ intellijPlatform {
             sinceBuild = "251"
         }
 
-        changeNotes = """
-      Initial version
-    """.trimIndent()
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+
+       
+
     }
+
+    signing {
+        certificateChain = providers.environmentVariable("CERTIFICATE_CHAIN")
+        privateKey = providers.environmentVariable("PRIVATE_KEY")
+        password = providers.environmentVariable("PRIVATE_KEY_PASSWORD")
+    }
+
+    publishing {
+        token = providers.environmentVariable("PUBLISH_TOKEN")
+        channels = providers.gradleProperty("pluginVersion")
+        .map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+}
+
+changelog {
+    groups.empty()
+    repositoryUrl = providers.gradleProperty("pluginRepositoryUrl")
 }
 
 tasks {
@@ -46,5 +76,8 @@ tasks {
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "21"
+    }
+     publishPlugin {
+        dependsOn(patchChangelog)
     }
 }
