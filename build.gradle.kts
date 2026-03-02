938a0d9e96770d66d689108d85f44a5b63fd3b48
CrhistianMRe <came.29meca@gmail.com>
2026-03-02 20:24:31 +0800

    feat: expand plugin compatiblity stating from 223.x

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "251"
+            sinceBuild = "223"
         }
 
         val changelog = project.changelog // local variable for configuration cache compatibility
